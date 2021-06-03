123e9414d5fdc82133cadec3a47cc5b47f5a1bd9
fazemodz <hike820@gmail.com>
2021-06-03 12:31:36 +0100

    initial commit to dev branch

---
+++ modules/errorhandler/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function ErrorHandler() {
+    return (
+        <>
+            <div className="Error-Header-container">
+                <p>owo</p>
+            </div>
+        </>
+    )
+
+}
